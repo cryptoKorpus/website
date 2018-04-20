c57d5f85dc10e4d4e66ae479c2f2b23035bfa622
David Pereira <david.pereira@ocidental.pt>
2018-04-20 10:26:13 +0100

    mailEditor component

--- src/App.vue
+++ src/App.vue
@@ -1,209 +1,185 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      fixed
-      :clipped="$vuetify.breakpoint.lgAndUp"
-      app
-      v-model="drawer">
-      <v-list dense>
-        <template v-for="item in items">
-          <v-layout
-            row
-            v-if="item.heading"
-            align-center
-            :key="item.heading"
-          >
-            <v-flex xs6>
-              <v-subheader v-if="item.heading">
-                {{ item.heading }}
-              </v-subheader>
-            </v-flex>
-            <v-flex xs6 class="text-xs-center">
-              <a href="#!" class="body-2 black--text">EDIT</a>
-            </v-flex>
-          </v-layout>
-          <v-list-group
-            v-else-if="item.children"
-            v-model="item.model"
-            :key="item.text"
-            :prepend-icon="item.model ? item.icon : item['icon-alt']"
-            append-icon=""
-          >
-            <v-list-tile slot="activator">
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ item.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-list-tile
-              v-for="(child, i) in item.children"
-              :key="i"
-              @keyup="teste"
-            >
-              <v-list-tile-action v-if="child.icon">
-                <v-icon>{{ child.icon }}</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title>
-                  {{ child.text }}
-                </v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </v-list-group>
-          <v-list-tile v-else @click="teste" :key="item.text">
-            <v-list-tile-action>
-              <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>
-                {{ item.text }}
-              </v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </template>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar
-      color="blue darken-3"
-      dark
-      app
-      :clipped-left="$vuetify.breakpoint.lgAndUp"
-      fixed
-    >
-      <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
-        <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-        <span class="hidden-sm-and-down">Blockmail</span>
-      </v-toolbar-title>
-      <v-text-field
-        flat
-        solo-inverted
-        prepend-icon="search"
-        label="Search"
-        class="hidden-sm-and-down"
-        v-model="filterText"
-      ></v-text-field>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>notifications</v-icon>
-      </v-btn>
-    </v-toolbar>
-    <v-content>
- <v-list two-line>
-          <template v-for="(item, index) in getEmails()">
-            <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
-            <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
-            <v-list-tile avatar v-else :key="item.title" @click="teste">
-            <v-avatar>
-              <v-icon>{{item.avatar}}</v-icon></v-avatar>
-              <v-list-tile-content>
-                <v-list-tile-title v-html="item.title"></v-list-tile-title>
-                <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
-              </v-list-tile-content>
-            </v-list-tile>
-          </template>
-        </v-list>
-    </v-content>
-    <v-btn
-      fab
-      bottom
-      right
-      color="pink"
-      dark
-      fixed
-      @click.stop="dialog = !dialog"
-    >
-      <v-icon>add</v-icon>
-    </v-btn>
-    <v-dialog v-model="dialog" width="800px">
-      <v-card>
-        <v-card-title
-          class="grey lighten-4 py-4 title"
-        >
-          Create contact
-        </v-card-title>
-        <v-container grid-list-sm class="pa-4">
-          <v-layout row wrap>
-            <v-flex xs12 align-center justify-space-between>
-              <v-layout align-center>
-                <v-avatar size="40px" class="mr-3">
-                  <img
-                    src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png"
-                    alt=""
-                  >
-                </v-avatar>
-                <v-text-field
-                  placeholder="Name"
-                ></v-text-field>
-              </v-layout>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                prepend-icon="business"
-                placeholder="Company"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs6>
-              <v-text-field
-                placeholder="Job title"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="mail"
-                placeholder="Email"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                type="tel"
-                prepend-icon="phone"
-                placeholder="(000) 000 - 0000"
-                mask="phone"
-              ></v-text-field>
-            </v-flex>
-            <v-flex xs12>
-              <v-text-field
-                prepend-icon="notes"
-                placeholder="Notes"
-              ></v-text-field>
-            </v-flex>
-          </v-layout>
-        </v-container>
-        <v-card-actions>
-          <v-btn flat color="primary">More</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
-          <v-btn flat @click="dialog = false">Save</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-  </v-app>
+    <v-app id="inspire">
+    
+<v-navigation-drawer fixed :clipped="$vuetify.breakpoint.lgAndUp" right app v-model="readBMail">
+ <mail-editor id="mailViewr"></mail-editor>
+</v-navigation-drawer>
+    
+        <v-navigation-drawer fixed :clipped="$vuetify.breakpoint.lgAndUp" app v-model="drawer">
+            <v-list dense>
+                <template v-for="item in items">
+                    <v-layout row v-if="item.heading" align-center :key="item.heading">
+                        <v-flex xs6>
+                            <v-subheader v-if="item.heading">
+                                {{ item.heading }}
+                            </v-subheader>
+                        </v-flex>
+                        <v-flex xs6 class="text-xs-center">
+                            <a href="#!" class="body-2 black--text">EDIT</a>
+                        </v-flex>
+                    </v-layout>
+                    <v-list-group v-else-if="item.children" v-model="item.model" :key="item.text" :prepend-icon="item.model ? item.icon : item['icon-alt']"
+                        append-icon="">
+                        <v-list-tile slot="activator">
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ item.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                        <v-list-tile v-for="(child, i) in item.children" :key="i" @keyup="teste">
+                            <v-list-tile-action v-if="child.icon">
+                                <v-icon>{{ child.icon }}</v-icon>
+                            </v-list-tile-action>
+                            <v-list-tile-content>
+                                <v-list-tile-title>
+                                    {{ child.text }}
+                                </v-list-tile-title>
+                            </v-list-tile-content>
+                        </v-list-tile>
+                    </v-list-group>
+                    <v-list-tile v-else @click="teste" :key="item.text">
+                        <v-list-tile-action>
+                            <v-icon>{{ item.icon }}</v-icon>
+                        </v-list-tile-action>
+                        <v-list-tile-content>
+                            <v-list-tile-title>
+                                {{ item.text }}
+                            </v-list-tile-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar color="blue darken-3" dark app :clipped-left="$vuetify.breakpoint.lgAndUp" fixed>
+            <v-toolbar-title style="width: 300px" class="ml-0 pl-3">
+                <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+                <span class="hidden-sm-and-down">Blockmail</span>
+            </v-toolbar-title>
+            <v-text-field flat solo-inverted prepend-icon="search" label="Search" class="hidden-sm-and-down" v-model="filterText"></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+                <v-icon>notifications</v-icon>
+            </v-btn>
+        </v-toolbar>
+        <v-content>
+            <v-list two-line>
+                <template v-for="(item, index) in getEmails()">
+                    <v-subheader v-if="item.header" :key="item.header">{{ item.header }}</v-subheader>
+                    <v-divider v-else-if="item.divider" :inset="item.inset" :key="index"></v-divider>
+                    <v-list-tile avatar v-else :key="item.title" @click="readBMail=!readBMail">
+                        <v-avatar>
+                            <v-icon>{{item.avatar}}</v-icon>
+                        </v-avatar>
+                        <v-list-tile-content>
+                            <v-list-tile-title v-html="item.title"></v-list-tile-title>
+                            <v-list-tile-sub-title v-html="item.subtitle"></v-list-tile-sub-title>
+                        </v-list-tile-content>
+                    </v-list-tile>
+                </template>
+            </v-list>
+        </v-content>
+        <v-btn fab bottom right color="pink" dark fixed @click.stop="newEmail">
+            <v-icon>add</v-icon>
+        </v-btn>
+        <v-dialog v-model="dialog" width="800px">
+            <v-card>
+                <v-card-title class="grey lighten-4 py-4 title">
+                    Create contact
+                </v-card-title>
+                <v-container grid-list-sm class="pa-4">
+                    <v-layout row wrap>
+                        <v-flex xs12 align-center justify-space-between>
+                            <v-layout align-center>
+                                <v-avatar size="40px" class="mr-3">
+                                    <img src="//ssl.gstatic.com/s2/oz/images/sge/grey_silhouette.png" alt="">
+                                </v-avatar>
+                                <v-text-field placeholder="Name"></v-text-field>
+                            </v-layout>
+                        </v-flex>
+                        <v-flex xs6>
+                            <v-text-field prepend-icon="business" placeholder="Company"></v-text-field>
+                        </v-flex>
+                        <v-flex xs6>
+                            <v-text-field placeholder="Job title"></v-text-field>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-text-field prepend-icon="mail" placeholder="Email"></v-text-field>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-text-field type="tel" prepend-icon="phone" placeholder="(000) 000 - 0000" mask="phone"></v-text-field>
+                        </v-flex>
+                        <v-flex xs12>
+                            <v-text-field prepend-icon="notes" placeholder="Notes"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+                <v-card-actions>
+                    <v-btn flat color="primary">More</v-btn>
+                    <v-spacer></v-spacer>
+                    <v-btn flat color="primary" @click="dialog = false">Cancel</v-btn>
+                    <v-btn flat @click="dialog = false">Save</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <v-dialog v-model="composeEmail">
+            <v-card>
+                <v-card-title class="grey lighten-4 py-4 title"> Compose email
+                </v-card-title>
+                <v-container grid-list-sm>
+                    <v-layout row wrap>
+                        <mail-editor id="mailWriter"></mail-editor>
+                    </v-layout>
+                </v-container>
+                <v-card-actions>
+                    <v-btn flat color="primary" @click="close">Cancel</v-btn>
+                    <v-btn flat @click="sendEmail">Send</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-app>
 </template>
 
 <script>
+import MailEditor from "./components/MailEditor.vue";
+
 export default {
+  components: {
+    MailEditor
+  },
+  mounted() {},
   data: () => ({
+    editor: null,
     filterText: null,
+    composeEmail: false,
     dialog: false,
     drawer: null,
+    readBMail: false,
     originalEmails: null,
     emails: [
-      { header: "Today" },
+      {
+        header: "Today"
+      },
       {
         avatar: "person",
         title: "Brunch this weekend?",
         subtitle:
           "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
       },
-      { divider: true, inset: true },
+      {
+        divider: true,
+        inset: true
+      },
       {
         avatar: "person",
         title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
         subtitle:
           "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
       },
-      { divider: true, inset: true },
+      {
+        divider: true,
+        inset: true
+      },
       {
         avatar: "person",
         title: "Oui oui",
@@ -212,24 +188,51 @@ export default {
       }
     ],
     items: [
-      { icon: "email", text: "Inbox" },
-      { icon: "mail_outline", text: "Sent Email" },
-      { icon: "contacts", text: "Contacts" },
+      {
+        icon: "email",
+        text: "Inbox"
+      },
+      {
+        icon: "mail_outline",
+        text: "Sent Email"
+      },
+      {
+        icon: "contacts",
+        text: "Contacts"
+      },
       {
         icon: "keyboard_arrow_up",
         "icon-alt": "keyboard_arrow_down",
         text: "Folders",
         model: true,
-        children: [{ icon: "add", text: "Create folders" }]
+        children: [
+          {
+            icon: "add",
+            text: "Create folders"
+          }
+        ]
       },
       {
         icon: "keyboard_arrow_up",
         "icon-alt": "keyboard_arrow_down",
         text: "More",
         model: false,
-        children: [{ text: "Import" }, { text: "Export" }, { text: "Print" }]
+        children: [
+          {
+            text: "Import"
+          },
+          {
+            text: "Export"
+          },
+          {
+            text: "Print"
+          }
+        ]
       },
-      { icon: "settings", text: "Settings" }
+      {
+        icon: "settings",
+        text: "Settings"
+      }
     ]
   }),
   props: {
@@ -242,8 +245,7 @@ export default {
       } else {
         var searchTxt = this.filterText;
         var filtered = this.emails.filter(mail => {
-          if (!mail.title) 
-          return false;
+          if (!mail.title) return false;
           return mail.title
             .toString()
             .toLowerCase()
@@ -251,6 +253,16 @@ export default {
         });
         return filtered;
       }
+    },
+    newEmail() {
+      this.composeEmail = !this.composeEmail;
+    },
+    close() {
+      this.composeEmail = false;
+      this.editor.setData("");
+    },
+    sendEmail() {
+      console.log("Sending email");
     },
 
     teste() {
---
+++ src/components/MailEditor.vue
@@ -0,0 +1,32 @@
+<template class="bmailEditor">
+
+  <textarea :id="id">
+ 
+</textarea>
+
+</template>
+
+<script>
+export default {
+    props:["id"],
+  mounted() {
+    ClassicEditor.create(document.querySelector("#"+this.$props.id))
+      .then(editor => {
+        console.log(editor);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
+
+<style>
+.bmailEditor {
+  width: 100%;
+  height: 100%;
+  
+}
+</style>
+
+
